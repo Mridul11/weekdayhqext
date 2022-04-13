be6eefc8d6f9a70486474db147db87470c3d1076
mishra_mridul <mridulmishra2991@gmail.com>
2022-04-13 08:18:20 +0530

    feat:setup chrome extension with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'weekdaychromeext';
+  data: any = [];
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.usersService.callUsers().subscribe((data) => this.data = data);
+  }
 }
